dbf249861eab002b08423667fa91509116952147
luca de Carvalho <luca.de.carvalho@gmail.com>
2019-11-12 18:43:46 +0100

    stuff

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+    LabelListController lbc;
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("view/sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1820, 1080));
         AnchorPane labelListPane= (AnchorPane) root.lookup("#LabelListPane");
-        AnchorPane label1= FXMLLoader.load(getClass().getResource("view/LabelPanel.fxml"));
-        labelListPane.getChildren().add(label1);
+        lbc= new LabelListController(labelListPane);
+        lbc.add(new Label("test2",new Color(0.3,0.2,0.5,1)));
+
         primaryStage.show();
     }
 
