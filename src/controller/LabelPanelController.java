2bf11f47711beb8a7b320f28837bce07476b15b8
goturak <luca.de.carvalho@gmail.com>
2019-11-13 00:05:39 +0100

    done

--- src/controller/LabelPanelController.java
+++ src/controller/LabelPanelController.java
@@ -1,5 +1,8 @@
 package controller;
 
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 
@@ -20,17 +23,34 @@ public class LabelPanelController implements Initializable {
     private AnchorPane myRoot;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        System.out.println(location);
+
     }
 
     public void setMyRoot(AnchorPane myRoot) {
         this.myRoot = myRoot;
         text= (TextField) myRoot.lookup("#labelText");
-        text.setText(label.getName());
+        text.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue ov, Boolean t, Boolean t1) {
+
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (text.isFocused() && !text.getText().isEmpty()) {
+                            text.selectAll();
+                        }
+                    }
+                });
+            }
+
+
+        });
+
+
         text.textProperty().addListener((observable, oldValue, newValue) -> {
             label.setName(newValue);
         });
-
+        text.requestFocus();
         colorPicker= (ColorPicker) myRoot.lookup("#colorPicker");
         colorPicker.setValue(label.getColor());
     }
