89bad05dc60079d9b8989ff978911a396aee8462
luca de Carvalho <luca.de.carvalho@gmail.com>
2019-11-12 18:01:51 +0100

    added stufff

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,19 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view/ihm.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view/sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1820, 1080));
+        AnchorPane labelListPane= (AnchorPane) root.lookup("#LabelListPane");
+        AnchorPane label1= FXMLLoader.load(getClass().getResource("view/LabelPanel.fxml"));
+        labelListPane.getChildren().add(label1);
         primaryStage.show();
     }
 
